9f3bb8ce191e4089e4d60a31b74dd0cddbf266d5
Stephen John Raymundo <bayan.mahal@live.com>
2018-10-16 21:52:34 +0800

    implement add, delete, getAll and update php files

---
+++ _system/php/api/categories/delete.php
@@ -0,0 +1,42 @@
+<?php
+
+# Set database parameters
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+# Retrieve POST parameters
+$category_id = $_POST['category_id'];
+
+# Check parameters if null
+if (isset($category_id)) {
+
+    try {
+
+        # Connect to Database
+        $conn = new PDO("mysql:host=$servername;dbname=rtl_v1", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        # Perform SQL Query
+        $sql = "DELETE FROM categories WHERE category_id = $category_id";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        # Print success
+        echo json_encode((object)[
+            'success' => true
+        ]);
+
+    } catch (PDOException $e) {
+
+        echo json_encode((object)[
+            'success' => false,
+            'message' => "Connection failed: " . $e->getMessage()
+        ]);
+
+    }
+}
+else {
+    echo 'values not set';
+}
+
---
+++ _system/php/api/categories/update.php
@@ -0,0 +1,45 @@
+<?php
+
+# Set database parameters
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+# Retrieve POST parameters
+$category_id = $_POST['category_id'];
+$category_name = $_POST['category_name'];
+$category_description = $_POST['category_description'];
+$category_active = $_POST['category_active'];
+
+# Check parameters if null
+if (isset($category_id) && isset($category_name) && isset($category_description) && isset($category_active)) {
+
+    try {
+
+        # Connect to Database
+        $conn = new PDO("mysql:host=$servername;dbname=rtl_v1", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        # Perform SQL Query
+        $sql = "UPDATE categories SET category_name='$category_name', category_description='$category_description', category_active='$category_active' WHERE category_id='$category_id'";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        # Print success
+        echo json_encode((object)[
+            'success' => true
+        ]);
+
+    } catch (PDOException $e) {
+
+        echo json_encode((object)[
+            'success' => false,
+            'message' => "Connection failed: " . $e->getMessage()
+        ]);
+
+    }
+}
+else {
+    echo 'values not set';
+}
+
