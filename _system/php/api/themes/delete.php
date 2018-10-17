fa6384cd17a3b5316589b981061b9e48f50766ea
ArcIX <rc2582739@gmail.com>
2018-10-17 14:22:39 +0800

    Add untested themes php functions

---
+++ _system/php/api/themes/delete.php
@@ -0,0 +1,42 @@
+<?php
+
+# Set database parameters
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+
+# Retrieve POST parameters
+$theme_id = $_POST['theme_id'];
+
+# Check parameters if null
+if (isset($theme_id)) {
+
+    try {
+
+        # Connect to Database
+        $conn = new PDO("mysql:host=$servername;dbname=rtl_v1", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        # Perform SQL Query
+        $sql = "DELETE FROM themes WHERE theme_id = $theme_id";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        # Print success
+        echo json_encode((object)[
+            'success' => true
+        ]);
+
+    } catch (PDOException $e) {
+
+        echo json_encode((object)[
+            'success' => false,
+            'message' => "Connection failed: " . $e->getMessage()
+        ]);
+
+    }
+}
+else {
+    echo 'values not set';
+}
+
---
+++ _system/php/api/themes/update.php
@@ -0,0 +1,44 @@
+<?php
+
+# Set database parameters
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+
+# Retrieve POST parameters
+$theme_id = $_POST['theme_id'];
+$theme_description = $_POST['theme_description'];
+$theme_image = $_POST['theme_image'];
+
+# Check parameters if null
+if (isset($theme_description) && isset($theme_image)) {
+
+    try {
+
+        # Connect to Database
+        $conn = new PDO("mysql:host=$servername;dbname=rtl_v1", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        # Perform SQL Query
+        $sql = "UPDATE themes SET theme_description='$theme_description', theme_image='$theme_image' WHERE theme_id='$theme_id'";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        # Print success
+        echo json_encode((object)[
+            'success' => true
+        ]);
+
+    } catch (PDOException $e) {
+
+        echo json_encode((object)[
+            'success' => false,
+            'message' => "Connection failed: " . $e->getMessage()
+        ]);
+
+    }
+}
+else {
+    echo 'values not set';
+}
+
