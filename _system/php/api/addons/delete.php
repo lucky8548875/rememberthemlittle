f7f462062956ef639f54b8a857302328381e1a79
ArcIX <rc2582739@gmail.com>
2018-10-17 13:27:28 +0800

    Add untested addons php functions

---
+++ _system/php/api/addons/delete.php
@@ -0,0 +1,42 @@
+<?php
+
+# Set database parameters
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+
+# Retrieve POST parameters
+$addon_id = $_POST['addon_id'];
+
+# Check parameters if null
+if (isset($addon_id)) {
+
+    try {
+
+        # Connect to Database
+        $conn = new PDO("mysql:host=$servername;dbname=rtl_v1", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        # Perform SQL Query
+        $sql = "DELETE FROM addons WHERE addon_id = $addon_id";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        # Print success
+        echo json_encode((object)[
+            'success' => true
+        ]);
+
+    } catch (PDOException $e) {
+
+        echo json_encode((object)[
+            'success' => false,
+            'message' => "Connection failed: " . $e->getMessage()
+        ]);
+
+    }
+}
+else {
+    echo 'values not set';
+}
+
