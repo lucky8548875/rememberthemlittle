db5bd471a280005659278940697e9443d32ce210
Stephen John Raymundo <bayan.mahal@live.com>
2018-10-20 10:06:56 +0800

    update for booking

---
+++ _system/php/api/booking/getAll.php
@@ -0,0 +1,35 @@
+<?php
+
+# Set database parameters
+$servername = "localhost";  
+$username = "root";
+$password = "root";
+
+try {
+
+    # Connect to Database
+    $conn = new PDO("mysql:host=$servername;dbname=rtl_v1", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    # Perform SQL Query
+    $stmt = $conn->prepare('SELECT * FROM bookings');
+    $stmt->execute();
+
+    # Fetch Result
+    $result = $stmt->fetchAll();
+    
+     # Print Result in JSON Format
+     echo json_encode((object)[
+        'success' => true,
+        'data' => $result
+     ],JSON_NUMERIC_CHECK);
+
+    }
+catch(PDOException $e)
+    {
+        echo json_encode((object)[
+            'success' => false,
+            'message' => "Connection failed: " . $e->getMessage()
+        ]);
+    }
+?>
