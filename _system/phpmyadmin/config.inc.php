761eced8bd301d30f6f3c7aa5cde299d5418d634
Stephen John Raymundo <bayan.mahal@live.com>
2018-10-29 12:37:54 +0800

    add phpmyadmin

--- _system/phpmyadmin/config.inc.php
+++ _system/phpmyadmin/config.inc.php
@@ -28,9 +28,9 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = 'us-cdbr-iron-east-01.cleardb.net';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 /**
  * phpMyAdmin configuration storage settings.
