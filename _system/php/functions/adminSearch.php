4f4fc3351d424d6f5b79d60d85975cf80d4c223e
ArcIX <rc2582739@gmail.com>
2019-01-07 14:18:13 +0800

    Set limit for queries to return and remove require_once for token

--- _system/php/functions/adminSearch.php
+++ _system/php/functions/adminSearch.php
@@ -2,7 +2,6 @@
 
 # Set database parameters
 require_once $_SERVER['DOCUMENT_ROOT'].'/_system/php/connection/db_connection.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/_system/php/functions/checkAdminToken.php';
 
 $query = $_GET['query'];
 
@@ -15,9 +14,9 @@ if(isset($query)){
 
         # Perform SQL Query
         $stmt = $conn->prepare(
-            "SELECT 'accounts' as source_table, account_id, NULL as booking_id, account_name, NULL as subject_name, NULL as booking_date FROM accounts WHERE account_id LIKE '%$query%' OR account_name LIKE '%$query%'
+            "SELECT 'accounts' as source_table, account_id, NULL as booking_id, account_name, NULL as subject_name, NULL as booking_date FROM accounts WHERE account_id LIKE '%$query%' OR account_name LIKE '%$query%' LIMIT 5
             UNION
-            SELECT 'bookings' as source_table, b.account_id, b.booking_id, a.account_name, b.subject_name, b.booking_date FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE b.booking_id LIKE '%$query%' OR a.account_name LIKE '%$query%' OR b.subject_name LIKE '%$query%'"
+            SELECT 'bookings' as source_table, b.account_id, b.booking_id, a.account_name, b.subject_name, b.booking_date FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE b.booking_id LIKE '%$query%' OR a.account_name LIKE '%$query%' OR b.subject_name LIKE '%$query%' LIMIT 5" 
         );
         $stmt->execute();
 
