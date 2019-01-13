42ac8bf382839aa9b60fce850c302a1b1fec3aa0
ArcIX <rc2582739@gmail.com>
2019-01-13 21:26:34 +0800

    Remove unnecessary lines of code

--- _system/php/api/customer/getGender.php
+++ _system/php/api/customer/getGender.php
@@ -3,42 +3,37 @@
 require_once $_SERVER['DOCUMENT_ROOT'].'/_system/php/connection/db_connection.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/_system/php/functions/checkAdminToken.php';
 
-$start_date = $_GET['start_date'];
-$end_date = $_GET['end_date'];
-if(isset($start_date) && isset($end_date))
+try
 {
-    try
-    {
-        # Connect to Database
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    # Connect to Database
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    # Perform SQL Query
+    // $stmt = $conn->prepare("SELECT DATE(account_created) as date, COUNT(account_id) as count FROM accounts WHERE account_created BETWEEN $start_date AND $end_date GROUP BY date");
+    $stmt = $conn->prepare(
+        "SELECT 
+            account_gender as gender, 
+            COUNT(account_id) as count 
+        FROM accounts   
+        GROUP BY gender"
+    );
+    $stmt->execute();
+
+    # Fetch Result
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
-        # Perform SQL Query
-        // $stmt = $conn->prepare("SELECT DATE(account_created) as date, COUNT(account_id) as count FROM accounts WHERE account_created BETWEEN $start_date AND $end_date GROUP BY date");
-        $stmt = $conn->prepare(
-            "SELECT 
-                account_gender as gender, 
-                COUNT(account_id) as count 
-            FROM accounts   
-            GROUP BY gender"
-        );
-        $stmt->execute();
-    
-        # Fetch Result
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        # Print Result in JSON Format
-        echo json_encode((object)[
-        'success' => true,
-        'data' => $result
-        ],JSON_NUMERIC_CHECK);
-    }
-    catch(PDOException $e)
-    {
-        echo json_encode((object)[
-            'success' => false,
-            'message' => "Connection failed: " . $e->getMessage()
-        ]);
-    }
+    # Print Result in JSON Format
+    echo json_encode((object)[
+    'success' => true,
+    'data' => $result
+    ],JSON_NUMERIC_CHECK);
+}
+catch(PDOException $e)
+{
+    echo json_encode((object)[
+        'success' => false,
+        'message' => "Connection failed: " . $e->getMessage()
+    ]);
 }
 ?>
