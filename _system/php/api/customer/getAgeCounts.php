18c9a6a89809b7f1a55c097bf23de9aaaaaaf6f4
ArcIX <rc2582739@gmail.com>
2019-01-13 21:36:02 +0800

    Add getAgeCounts.php under _system/php/api/customer

---
+++ _system/php/api/customer/getAgeCounts.php
@@ -0,0 +1,39 @@
+<?php
+# Set database parameters
+require_once $_SERVER['DOCUMENT_ROOT'].'/_system/php/connection/db_connection.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/_system/php/functions/checkAdminToken.php';
+
+try
+{
+    # Connect to Database
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    # Perform SQL Query
+    // $stmt = $conn->prepare("SELECT DATE(account_created) as date, COUNT(account_id) as count FROM accounts WHERE account_created BETWEEN $start_date AND $end_date GROUP BY date");
+    $stmt = $conn->prepare(
+        "SELECT 
+            TIMESTAMPDIFF(YEAR, account_birthdate, CURDATE()) as age, 
+            COUNT(account_id) as count 
+        FROM accounts   
+        GROUP BY age"
+    );
+    $stmt->execute();
+
+    # Fetch Result
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    # Print Result in JSON Format
+    echo json_encode((object)[
+    'success' => true,
+    'data' => $result
+    ],JSON_NUMERIC_CHECK);
+}
+catch(PDOException $e)
+{
+    echo json_encode((object)[
+        'success' => false,
+        'message' => "Connection failed: " . $e->getMessage()
+    ]);
+}
+?>
