376bd00233109c859f9d43d2e0308622c25e7ab8
ArcIX <rc2582739@gmail.com>
2019-01-16 17:08:00 +0800

    getAgeCounts.php now groups results by age range instead of age

--- _system/php/api/customer/getAgeCounts.php
+++ _system/php/api/customer/getAgeCounts.php
@@ -13,10 +13,16 @@ try
     // $stmt = $conn->prepare("SELECT DATE(account_created) as date, COUNT(account_id) as count FROM accounts WHERE account_created BETWEEN $start_date AND $end_date GROUP BY date");
     $stmt = $conn->prepare(
         "SELECT 
-            TIMESTAMPDIFF(YEAR, account_birthdate, CURDATE()) as age, 
+            CASE 
+                WHEN TIMESTAMPDIFF(YEAR, account_birthdate, CURDATE()) < 18 THEN 'Below 18' 
+                WHEN TIMESTAMPDIFF(YEAR, account_birthdate, CURDATE()) BETWEEN 18 AND 30 THEN '18-30' 
+                WHEN TIMESTAMPDIFF(YEAR, account_birthdate, CURDATE()) BETWEEN 31 AND 45 THEN '31-45' 
+                WHEN TIMESTAMPDIFF(YEAR, account_birthdate, CURDATE()) BETWEEN 46 AND 60 THEN '46-60' 
+                ELSE 'Over 60'
+            END AS age_range, 
             COUNT(account_id) as count 
         FROM accounts   
-        GROUP BY age"
+        GROUP BY age_range"
     );
     $stmt->execute();
 
