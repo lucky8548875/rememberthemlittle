52403a70852ca594b0df98bed0114e71f6c3b561
Carl Jason <elrax777costiniano@yahoo.com.ph>
2019-02-07 11:27:32 +0800

    gallery update 2

--- _system/js/components/gallery.js
+++ _system/js/components/gallery.js
@@ -4,15 +4,9 @@ var app = new Vue({
         wScroll: 0,
         scrollPlacing: 0,
         backgroundImage: "/_system/images/backgroundgallery.png",
-        images:['/_system/images/gallery/Picture 1 Display.jpg',
-        '/_system/images/gallery/Picture 2 Display.jpg',
-        "/_system/images/gallery/Picture 3 Display.jpg",
-        "/_system/images/gallery/Picture 4 Display.jpg",
-        "/_system/images/gallery/Picture4.jpg",
-        "/_system/images/gallery/Picture3.jpg",
-        "/_system/images/gallery/Picture1.jpg",
-        "/_system/images/gallery/Picture4.jpg",
-        "/_system/images/gallery/Picture3.jpg"]
+        images1:['/_system/images/gallery/Picture 1 Display.jpg','/_system/images/gallery/Picture 2 Display.jpg',"/_system/images/gallery/Picture 3 Display.jpg","/_system/images/gallery/Picture 4 Display.jpg","/_system/images/gallery/Picture4.jpg","/_system/images/gallery/Picture3.jpg","/_system/images/gallery/Picture1.jpg","/_system/images/gallery/Picture4.jpg","/_system/images/gallery/Picture3.jpg"],
+        images2:["/_system/images/gallery/Picture5.jpg","/_system/images/gallery/Picture6.jpg", "/_system/images/gallery/Picture7.jpg", "/_system/images/gallery/Picture5.jpg", "/_system/images/gallery/Picture5.jpg", "/_system/images/gallery/Picture5.jpg"],
+        images3:["/_system/images/gallery/Picture8.jpg","/_system/images/gallery/Picture9.jpg","/_system/images/gallery/Picture10.jpg","/_system/images/gallery/Picture10.jpg"],
     },
     methods: {
         scrollMoved: function() {
