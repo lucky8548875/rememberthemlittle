0caf6ded95d936d6e7dfcc0943803e5c7b2be729
Carl Jason <elrax777costiniano@yahoo.com.ph>
2019-02-27 22:09:59 +0800

    update reports for days

--- _system/php/api/salesreport/getByPage.php
+++ _system/php/api/salesreport/getByPage.php
@@ -31,29 +31,29 @@ try {
         if(isset($calendarMode)){
             if($calendarMode=="Day"){
                 if($searchbar != ''){
-                    $stmt = $conn->prepare("SELECT booking_date, SUM(booking_total_price) as bookings_total, account_name FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' AND account_name='$searchbar' GROUP BY account_name ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
+                    $stmt = $conn->prepare("SELECT booking_date, SUM(booking_total_price) as bookings_total, account_name FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' AND booking_date='$searchbar' GROUP BY account_name ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
                 }else{
                     $stmt = $conn->prepare("SELECT booking_date, SUM(booking_total_price) as bookings_total, account_name FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' GROUP BY account_name ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
                 }   
             }else if($calendarMode=="Week"){
-                if($searchbar != ''){
-                    $stmt = $conn->prepare("SELECT DATE_ADD(booking_date, INTERVAL(1-DAYOFWEEK(booking_date)) DAY) as weekstart, DATE_ADD(booking_date, INTERVAL(7-DAYOFWEEK(booking_date)) DAY) as weekend, SUM(booking_total_price) as bookings_total FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' AND account_name='$searchbar' GROUP BY WEEK(booking_date) ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
-                }else{
+                // if($searchbar != 'mm/dd/yyyy'){
+                //     $stmt = $conn->prepare("SELECT DATE_ADD(booking_date, INTERVAL(1-DAYOFWEEK(booking_date)) DAY) as weekstart, DATE_ADD(booking_date, INTERVAL(7-DAYOFWEEK(booking_date)) DAY) as weekend, SUM(booking_total_price) as bookings_total FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' AND booking_created='$searchbar' GROUP BY WEEK(booking_date) ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
+                // }else{
                     $stmt = $conn->prepare("SELECT DATE_ADD(booking_date, INTERVAL(1-DAYOFWEEK(booking_date)) DAY) as weekstart, DATE_ADD(booking_date, INTERVAL(7-DAYOFWEEK(booking_date)) DAY) as weekend, SUM(booking_total_price) as bookings_total FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' GROUP BY WEEK(booking_date) ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
-                }
+                //}
             }else if($calendarMode=="Month"){
-                if($searchbar != ''){
-                    $stmt = $conn->prepare("SELECT DATE_ADD(DATE_ADD(LAST_DAY(booking_date), INTERVAL 1 DAY), INTERVAL - 1 MONTH) as monthstart, LAST_DAY(booking_date) as monthend, SUM(booking_total_price) as bookings_total FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' AND account_name='$searchbar' GROUP BY extract(month from booking_date) ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
-                }else{
+                // if($searchbar != 'mm/dd/yyyy'){
+                //     $stmt = $conn->prepare("SELECT DATE_ADD(DATE_ADD(LAST_DAY(booking_date), INTERVAL 1 DAY), INTERVAL - 1 MONTH) as monthstart, LAST_DAY(booking_date) as monthend, SUM(booking_total_price) as bookings_total FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' AND booking_created='$searchbar' GROUP BY extract(month from booking_date) ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
+                // }else{
                     $stmt = $conn->prepare("SELECT DATE_ADD(DATE_ADD(LAST_DAY(booking_date), INTERVAL 1 DAY), INTERVAL - 1 MONTH) as monthstart, LAST_DAY(booking_date) as monthend, SUM(booking_total_price) as bookings_total FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' GROUP BY extract(month from booking_date) ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
-                }
+                //}
                 
             }else if($calendarMode=="Year"){
-                if($searchbar != ''){
-                    $stmt = $conn->prepare("SELECT YEAR(booking_date) as salesyear, SUM(booking_total_price) as bookings_total FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' AND account_name='$searchbar' GROUP BY YEAR(booking_date) ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
-                }else{
+                // if($searchbar != ''){
+                //     $stmt = $conn->prepare("SELECT YEAR(booking_date) as salesyear, SUM(booking_total_price) as bookings_total FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' AND booking_created='$searchbar' GROUP BY YEAR(booking_date) ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
+                // }else{
                     $stmt = $conn->prepare("SELECT YEAR(booking_date) as salesyear, SUM(booking_total_price) as bookings_total FROM bookings b INNER JOIN accounts a ON b.account_id = a.account_id WHERE booking_status='BOOKED' GROUP BY YEAR(booking_date) ORDER BY booking_created DESC LIMIT $records_display_startcount, 50");
-                }
+                //}
                 
             }
         }
